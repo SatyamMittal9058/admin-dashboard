929b02138ba1e82f5849baa8a7f289a24390a133
satyammittal9058 <mittal.satyam9058@gmail.com>
2023-12-02 13:37:58 +0530

    setup taiilwind and check

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+
+
+  useEffect(()=>{
+    const fetchdata=async ()=>{
+      try{
+        const data= await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
+        if(data.ok) {
+          const res=await data.json();
+          console.log(res);
+        }
+        else console.log("Data not found");
+      }catch(error){
+        console.error("Data not fount",error);
+      }
+    }
+    fetchdata();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <h1>hello world</h1>
     </div>
   );
 }
